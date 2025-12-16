af4705a644252526fb34a93d542a5ce58c726f37
sunxiaohui19891003-design <sunxiaohui19891003@gmail.com>
2025-12-16 10:44:31 +0900

    add favorite feature

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,26 +1,22 @@
 <template>
-  <div style="padding: 40px; max-width: 600px; margin: auto">
-    <h2>我的收藏</h2>
-    <button @click="goBack" style="margin-bottom: 20px">
-  返回微博
-</button>
-    <div v-if="weiboList.length === 0" style="color: #888">
-      暂无收藏
-    </div>
-
-    <ul>
-      <li v-for="w in weiboList" :key="w.id" style="margin-bottom: 12px">
-        {{ w.content }}
-        <button @click="toggleFavorite(w.id)" style="margin-left: 10px">
-          取消收藏
+    <div style="padding: 40px; max-width: 600px; margin: auto">
+        <h2>我的收藏</h2>
+        <button @click="goBack" style="margin-bottom: 20px">
+            返回微博
         </button>
-      </li>
-    </ul>
+        <div v-if="weiboList.length === 0" style="color: #888">
+            暂无收藏
+        </div>
 
-    <button style="margin-top: 20px" @click="goBack">
-      返回微博
-    </button>
-  </div>
+        <ul>
+            <li v-for="w in weiboList" :key="w.id" style="margin-bottom: 12px">
+                {{ w.content }}
+                <button @click="toggleFavorite(w.id)" style="margin-left: 10px">
+                    取消收藏
+                </button>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
@@ -32,32 +28,30 @@ axios.defaults.withCredentials = true
 
 const router = useRouter()
 const weiboList = ref([])
-function goBack() {
-  router.push('/')
-}
+
 async function loadFavorites() {
-  const res = await axios.get(
-    'https://miniweibo-backend.onrender.com/favorites'
-  )
-  weiboList.value = res.data
+    const res = await axios.get(
+        'https://miniweibo-backend.onrender.com/favorites'
+    )
+    weiboList.value = res.data
 }
 
 async function toggleFavorite(weiboId) {
-  await axios.post(
-    'https://miniweibo-backend.onrender.com/favorites',
-    null,
-    {
-      params: { weiboId }
-    }
-  )
-  loadFavorites()
+    await axios.post(
+        'https://miniweibo-backend.onrender.com/favorites',
+        null,
+        {
+            params: { weiboId }
+        }
+    )
+    loadFavorites()
 }
 
 function goBack() {
-  router.push('/')
+    router.push('/')
 }
 
 onMounted(() => {
-  loadFavorites()
+    loadFavorites()
 })
 </script>
